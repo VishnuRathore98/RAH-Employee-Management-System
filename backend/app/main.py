63c011eeb96183bc1a173b30b74f248179d78864
VishnuRathore98 <rathorevishnu666@gmail.com>
2025-08-23 23:01:41 +0530

    Add: backend -> CRUD operations

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,67 @@
-from fastapi import FastAPI
+from typing import List
+from uuid import UUID, uuid4
+from fastapi import FastAPI, HTTPException
+
+from models.models import Gender, Role, User, UserUpdateRequest
 
 app = FastAPI()
+
+db: List[User] = [
+    User(
+        id=uuid4(),
+        first_name="John",
+        last_name="Doe",
+        gender=Gender.male,
+        roles=[Role.employee],
+    ),
+    User(
+        id=uuid4(),
+        first_name="Hina",
+        last_name="Doe",
+        gender=Gender.female,
+        roles=[Role.manager],
+    ),
+]
 
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the RAH-EMS"}
+
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db
+
+
+@app.post("/api/v1/users")
+async def create_user(user: User):
+    db.append(user)
+    return {"id": user.id}
+
+
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+    for user in db:
+        if user_id == user.id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+        return user
+    raise HTTPException(status_code=404, detail="User not found!")
+
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return {"id": user.id}
+    raise HTTPException(
+        status_code=404, detail=f"User with id: {user_id} does not exists."
+    )
---
+++ backend/app/models/models.py
@@ -0,0 +1,32 @@
+from typing import Optional, List
+from uuid import UUID, uuid4
+from click import Option
+from pydantic import BaseModel, Field
+from enum import Enum
+
+
+class Role(str, Enum):
+    admin = "admin"
+    manager = "manager"
+    employee = "employee"
+
+
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+
+
+class User(BaseModel):
+    id: Optional[UUID] = Field(default_factory=uuid4)
+    first_name: str
+    middle_name: Optional[str] = None
+    last_name: str
+    gender: Gender
+    roles: List[Role]
+
+
+class UserUpdateRequest(BaseModel):
+    first_name: Optional[str]
+    middle_name: Optional[str]
+    last_name: Optional[str]
+    roles: Optional[List[Role]]
