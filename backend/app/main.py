adbe6ab61aefab0668d30caab1c8575ba21e83e0
VishnuRathore98 <rathorevishnu666@gmail.com>
2025-09-10 21:32:10 +0530

    fastapi testclient

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ from app.crud.database import engine
 from app.routes import employees, users, tasks, authentication, votes
 
 # Required FastAPI setup
-time.sleep(120)
+
 app = FastAPI()
 
 origins = ["*"]
---
+++ backend/tests/test_employees.py
@@ -0,0 +1,9 @@
+from app.main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+def test_root():
+    res = client.get("/")
+    print(res.json())
+    assert res.status_code == 200
