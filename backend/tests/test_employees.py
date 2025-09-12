116fc9e13617d14c6ca8c92010190c44d523581f
VishnuRathore98 <rathorevishnu666@gmail.com>
2025-09-12 22:15:25 +0530

    seperated the dummy db setup logic

--- backend/tests/test_employees.py
+++ backend/tests/test_employees.py
@@ -1,41 +1,5 @@
-from app.main import app
-from fastapi.testclient import TestClient
 from app.schemas import schemas
-import pytest
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from app.config.config import Settings
-from app.models.models import Base
-from app.crud.database import get_db
-
-settings = Settings()
-
-POSTGRES_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
-
-engine = create_engine(POSTGRES_DATABASE_URL)
-
-TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-def override_get_db():
-    db = TestSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-app.dependency_overrides[get_db] = override_get_db
-
-@pytest.fixture()
-def client():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    yield TestClient(app)
-
-#Base = declarative_base()
-
+from tests.dummy_database import client
 
 def test_root(client):
     res = client.get("/")
